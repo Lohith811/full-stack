76f2cba5e936b5e679df088d24a81a8c6b58de14
Lohith811 <vtu25508@veltech.edu.in>
2026-03-17 11:21:43 +0530

    Add files via upload

---
+++ task9-mvc/src/main/java/com/example/demo/Task9MvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task9MvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task9MvcApplication.class, args);
+	}
+
+}
